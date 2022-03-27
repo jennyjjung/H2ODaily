e5500cd4e45365c9a5fe26a928af59e2ceba3f60
annie-e <grace2hyun@gmail.com>
2022-03-26 23:20:10 -0400

    Second Commit

--- main.js
+++ main.js
@@ -55,21 +55,45 @@ function removeImg() {
 
 //reminder.html
 document.addEventListener("DOMContentLoaded", applyImg); 
+document.addEventListener("DOMContentLoaded", addCheckBox);
 
 let submit = document.getElementById("submit"); 
 let reminderDiv = document.getElementById("reminder__container");
+let checkBoxDiv = document.getElementById("checkmark__container");
+var wCount = localStorage.getItem("waterCount"); 
 
 function applyImg() {
     
-    for (let i = 0; i < localStorage.getItem("waterCount"); i++) {
+    for (let i = 0; i < wCount; i++) {
         let waterImage = document.createElement("IMG");
         waterImage.src = "images/watercup.png";
         waterImage.setAttribute("width", "100px");
         waterImage.setAttribute("height", "100px");
-        waterImage.setAttribute("id", "water" + localStorage.getItem("waterCount"));
+        waterImage.setAttribute("id", "water" + i);
         reminderDiv.appendChild(waterImage); 
     }
-   
 }
 
-submit.addEventListener("click", applyImg);
+function addCheckBox() {
+    for(let i = 0; i < wCount; i++) {
+        let checkBox = document.createElement("INPUT");
+        checkBox.setAttribute("type", "checkbox"); 
+        checkBox.setAttribute("id", "checkBox" + i);
+        checkBox.style.margin = "10px 45px 10px 45px";
+        checkBoxDiv.appendChild(checkBox);
+    }
+}
+
+function drinkWater() {
+    for(let i = 0; i < wCount; i++) {
+        let eachWaterCup = document.getElementById("water" + i);
+        let eachCheckBox = document.getElementById("checkBox" + i);
+
+        if(eachCheckBox.checked) {
+            eachWaterCup.src = "images/emptycup.png"; 
+            eachCheckBox.style.visibility = "hidden";
+        }
+    }
+ }
+
+submit.addEventListener("click", drinkWater);
